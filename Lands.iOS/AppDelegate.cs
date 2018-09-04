4ed40430b320d6b1435320fbc6c287e32010a83f
MAC <mac@MacBook-Pro-de-Gualter.local>
2018-09-04 01:45:59 +0100

    Aula 34 concluida!
    Está tudo funcionando como devia. Graças a Deus!

--- Lands.iOS/AppDelegate.cs
+++ Lands.iOS/AppDelegate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using FFImageLoading.Forms.Touch;
 using Foundation;
+using ImageCircle.Forms.Plugin.iOS;
 using UIKit;
 
 namespace Lands.iOS
@@ -23,7 +24,9 @@ namespace Lands.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+
             CachedImageRenderer.Init();
+            ImageCircleRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
